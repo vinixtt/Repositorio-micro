4be1dd2ba8987fa831e3551364e801d11f8c80a9
Vinicius Figueiredo <162514053+vinixtt@users.noreply.github.com>
2025-11-08 20:47:14 -0300

    feat: 2st question solution

---
+++ questao2.c
@@ -0,0 +1,21 @@
+void questao2(){
+	  while (1){
+		  Delay_us(1000000);
+
+		  GPIO_Write_Pin(GPIOA, PIN_6, HIGH);
+		  Delay_us(500000);
+		  GPIO_Write_Pin(GPIOA, PIN_6, LOW);
+
+		  Delay_us(1000000);
+
+		  GPIO_Write_Pin(GPIOA, PIN_6, HIGH);
+		  Delay_us(500000);
+		  GPIO_Write_Pin(GPIOA, PIN_6, LOW);
+
+		  Delay_us(500000);
+
+		  GPIO_Write_Pin(GPIOA, PIN_6, HIGH);
+		  Delay_us(500000);
+		  GPIO_Write_Pin(GPIOA, PIN_6, LOW);
+	  }
+}
